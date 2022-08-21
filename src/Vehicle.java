49dc80c8e604ffe10b2f136e7dc798bb2ca55616
Orlando <pereztwinning@gmail.com>
2022-08-21 15:13:10 -0400

    Initial commit

---
+++ src/Vehicle.java
@@ -0,0 +1,28 @@
+ class Vehicle {
+    int maxSpeed;
+
+    Vehicle() {
+        System.out.println("Vehicle constructor");
+    }
+
+    Vehicle(int maxSpeed) {
+        System.out.println("Vehicle constructor w/ speed");
+        this.maxSpeed = maxSpeed;
+    }
+}
+
+class Car extends Vehicle {
+
+    Car() {
+        super();
+
+        System.out.println("Car constructor");
+
+    }
+//    public void vroom() {
+//        super.vroom();
+//    }
+
+}
+
+
